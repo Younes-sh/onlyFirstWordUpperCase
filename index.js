c3dab3267e3e44ba646ac9ba1f28fa53db38a61a
Younes-sh <sheikhlaryounes@gmail.com>
2023-06-07 00:29:49 +0200

    added index.html & index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+const name1 = 'younes sheikhlar';
+const name2 = name1.toLocaleLowerCase()
+const cutFirstWord = name2.charAt().toLocaleUpperCase()
+const deletFirstWord = name2.slice(1)
+const result = cutFirstWord + deletFirstWord ;
+console.log(result)
